39ecf9b34945b2be6c5ae6e2153ebac398f635f7
Italo Costa <costa.italo.ti@gmail.com>
2023-09-25 08:56:36 -0300

    Route search

--- src/theMovieDb/urls.ts
+++ src/theMovieDb/urls.ts
@@ -1,6 +1,3 @@
 export const baseMoviesUrl : string = "https://api.themoviedb.org/3/movie/"
-export const searchMoviesUrl: string =`${baseMoviesUrl}search/movie/`;
+export const searchMoviesUrl: string =`https://api.themoviedb.org/3/search/movie`;
 export const searchMovieImgUrl: string = "https://image.tmdb.org/t/p/w500/"
-export const topRatedUrl = `${baseMoviesUrl}top_rated`;
-
-//const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
